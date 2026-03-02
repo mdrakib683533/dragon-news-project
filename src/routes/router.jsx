8aa6c7a1bab087ebb2fd747e5c36c288ee8c3331
rakibhossen123 <osmangoni87367@gmail.com>
2026-03-03 02:47:30 +0600

    dragon-news project start this time

---
+++ src/routes/router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layouts/HomeLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: HomeLayout,
+  },
+  {
+    path: "/auth",
+    element: <h1>Authentication layout</h1>,
+  },
+  {
+    path: "/news",
+    element: <h1>News layout</h1>,
+  },
+  {
+    path: "/*",
+    element: <h1>Error 404</h1>,
+  },
+]);
+export default router;
